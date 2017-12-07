731cca4552ca215493dcdfe611023717d70f444a
kovunov <anton.kovunov@gmail.com>
2017-12-07 12:53:03 -0500

    Updated Boards seeder and created databaseReadme file with instructions

--- database/seeds/BoardsTableSeeder.php
+++ database/seeds/BoardsTableSeeder.php
@@ -6,6 +6,7 @@ class BoardsTableSeeder extends Seeder
 {
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         \App\Board::truncate();
 
         $faker = \Faker\Factory::create();
@@ -16,5 +17,6 @@ class BoardsTableSeeder extends Seeder
                 'user_id' => $faker->randomElement(\App\User::all()->pluck('id')->toArray()),
             ]);
         }
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
